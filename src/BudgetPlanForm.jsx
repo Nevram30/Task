423db1f4f86ae57130fd95ae1252fe6e57df7471
Nevram30 <mars.paray@gmail.com>
2023-05-07 20:43:23 +0800

    refactor-v4

--- src/BudgetPlanForm.jsx
+++ src/BudgetPlanForm.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-function BudgetPlanForm({ newbudgetPlan, handleNewBudgetPlan, addBudget }) {
+function BudgetPlanForm({ newbudgetPlan, handleNewBudgetPlan }) {
   return (
     <div className="form mb-5">
       <label>Budget Amount:</label>
@@ -11,12 +11,6 @@ function BudgetPlanForm({ newbudgetPlan, handleNewBudgetPlan, addBudget }) {
         onChange={handleNewBudgetPlan}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
-      <button
-        onClick={addBudget}
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Add
-      </button>
     </div>
   );
 }
@@ -26,7 +20,6 @@ BudgetPlanForm.propTypes = {
     amount: PropTypes.number,
   }),
   handleNewBudgetPlan: PropTypes.func,
-  addBudget: PropTypes.func,
 };
 
 export default BudgetPlanForm;
